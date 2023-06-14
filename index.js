ee5d8672b7306ec345a0e30563cb20915a7761d6
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-06-14 12:22:26 +0530

    changing port

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post('/api/persons', (request, response) => {
 app.use(unknownEndpoint)
 
 // making app listen to the port 3001
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
