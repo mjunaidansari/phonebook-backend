7464d12a9d7f5d0f9545ba5a9f612470000e5139
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-06-14 12:38:00 +0530

    uibuild

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const unknownEndpoint = (request, response) => {
  
 //middleware are called in order they are taken into use
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
